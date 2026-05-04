1aa5f3e9bce2d4b1a6f029182e0de4eed009d3f2
aront11205-pixel <aront11205@gmail.com>
2026-05-03 21:34:30 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDFGy_CqybHAwZrrPXOHXRkLBEZB5zDmw",
+  apiKey: "AIzaSyBDFGy_CqybHAwZrrPXONXRkL8EZB5zOmw",
   authDomain: "luxebound-orders.firebaseapp.com",
   projectId: "luxebound-orders",
   storageBucket: "luxebound-orders.firebasestorage.app",
-  messagingSenderId: "891313099200",
-  appId: "1:891313099200:web:26ee2c0be0b82329a24625"
+  messagingSenderId: "892313099200",
+  appId: "1:892313099200:web:26ee2cd0a0b02329a24625"
 };
 
 const app = initializeApp(firebaseConfig);
